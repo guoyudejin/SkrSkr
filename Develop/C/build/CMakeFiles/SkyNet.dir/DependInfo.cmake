8ee2f3d586e87e32c02f64108ea11381e43bca75
何义乾 <16231306@bjtu.edu.cn>
2021-05-25 14:59:16 +0800

    add the simulation part using g++

---
+++ Develop/C/build/CMakeFiles/SkyNet.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/heyiqian/宽广/GitHub/skrskr/Develop/C/src/SkyNet.cpp" "/Users/heyiqian/宽广/GitHub/skrskr/Develop/C/build/CMakeFiles/SkyNet.dir/src/SkyNet.cpp.o"
+  "/Users/heyiqian/宽广/GitHub/skrskr/Develop/C/src/main.cpp" "/Users/heyiqian/宽广/GitHub/skrskr/Develop/C/build/CMakeFiles/SkyNet.dir/src/main.cpp.o"
+  "/Users/heyiqian/宽广/GitHub/skrskr/Develop/C/src/transform.cpp" "/Users/heyiqian/宽广/GitHub/skrskr/Develop/C/build/CMakeFiles/SkyNet.dir/src/transform.cpp.o"
+  "/Users/heyiqian/宽广/GitHub/skrskr/Develop/C/src/utils.cpp" "/Users/heyiqian/宽广/GitHub/skrskr/Develop/C/build/CMakeFiles/SkyNet.dir/src/utils.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  ".././include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Develop/C/build/CMakeFiles/SkyNet.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SkyNet.dir/src/SkyNet.cpp.o"
+  "CMakeFiles/SkyNet.dir/src/main.cpp.o"
+  "CMakeFiles/SkyNet.dir/src/transform.cpp.o"
+  "CMakeFiles/SkyNet.dir/src/utils.cpp.o"
+  "SkyNet"
+  "SkyNet.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SkyNet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
